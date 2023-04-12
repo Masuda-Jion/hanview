ec07f13dcf5fa1b4d9da1ce178c0900253db751e
Masuda-Jion <masuda0311jion@gmail.com>
2023-04-12 20:07:50 +0000

    [Finish] カラム追加

--- db/migrate/20230412171007_create_reviews.rb
+++ db/migrate/20230412171007_create_reviews.rb
@@ -1,7 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-
+      t.integer :customer_id
+      t.integer :menu_id
+      t.string :title
+      t.string :star
+      t.text :content
       t.timestamps
     end
   end
