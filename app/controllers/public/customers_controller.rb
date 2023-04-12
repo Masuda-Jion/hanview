c62d04be913a35d54fc364ea22e9f2864a6e2eca
Masuda-Jion <masuda0311jion@gmail.com>
2023-04-12 20:54:56 +0000

    [add] コントローラー

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,16 @@
+class Public::CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def check
+  end
+
+  def follows
+  end
+
+  def followers
+  end
+end
---
+++ app/controllers/public/reviews_controller.rb
@@ -0,0 +1,10 @@
+class Public::ReviewsController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
