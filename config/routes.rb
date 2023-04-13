32b8270bbd592c48052106a189a32d20cac41342
Masuda-Jion <masuda0311jion@gmail.com>
2023-04-13 09:49:26 +0000

    [Finish] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,66 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'reviews/index'
-    get 'reviews/edit'
-    get 'reviews/show'
-  end
-  namespace :public do
-    get 'deliveries/index'
-    get 'deliveries/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/index'
-    get 'orders/show'
-    get 'orders/done'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
+
+  # 顧客用
+  devise_for :customers, skip: [:passwords], controllers: {
+    #registrationsコントローラー
+    registrations: "public/registrations",
+    #sessionsコントローラー
+    sessions: "public/sessions"
+  }
+
+  # 管理者用
+  devise_for :admin, controllers: {
+    #sessionsコントローラー
+    sessions: "admin/sessions"
+  }
+
+  scope module: :public do
+    #homeコントローラー
+    root to: 'homes#top'
+    get 'homes/about'
+    #menusコントローラー
+    resources:menus, only: [:index, :show, :create]
+    get 'customers/mypage' => 'customers#show'
+    get 'customers/mypage/edit' => 'customers#edit'
+    patch 'customers/mypage' => 'customers#update'
     get 'customers/check'
+    patch 'customers/withdraw'
     get 'customers/follows'
     get 'customers/followers'
+    #cart_itemsコントローラー
+    resources:cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all'
+    #ordersコントローラー
+    resources:orders, only: [:new,:create,:index,:show]
+    post 'orders/check'
+    get 'orders/done'
+    #deliveriesコントローラー
+    resources:deliveries, only: [:index, :edit, :create, :update, :destroy]
+    #reviewsコントローラー
+    resources:reviews, only: [:index, :edit, :show, :create, :update, :destroy]
+    post 'reviews/comment'
+    post 'reviews/like'
+    delete 'reviews/unlike'
+    #relationshipsコントローラー
+    resources:relationships, only: [:create, :destroy]
   end
-  namespace :public do
-    get 'menus/index'
-    get 'menus/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
   namespace :admin do
-    get 'orders/show'
+    #homesコントローラー
+    get '/' => 'homes#top'
+    #menusコントローラー
+    resources:menus, only:[:new, :index, :create, :show, :edit, :update]
+    #genresコントローラー
+    resources:genres, only: [:index, :create, :edit, :update]
+    #customersコントローラー
+    resources:customers, only:[:index, :show, :edit, :update]
+    #ordersコントローラー
+    resources:orders, only:[:show, :update]
+    #order_detailsコントローラー
+    resources:order_details, only:[:update]
+    #reviewsコントローラー
+    resources:reviews, only:[:destroy]
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'menus/new'
-    get 'menus/index'
-    get 'menus/show'
-    get 'menus/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  devise_for :admins
-  devise_for :customers
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
