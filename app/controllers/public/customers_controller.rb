eb6ecd54970a608091423da6258d7416e46fad6f
Masuda-Jion <masuda0311jion@gmail.com>
2023-04-14 16:08:34 +0000

    [Add] マイページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,24 @@
 class Public::CustomersController < ApplicationController
+
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_mypage_path
+    else
+      render :edit
+    end
   end
 
   def check
+    @customer = current_customer
   end
 
   def follows
@@ -13,4 +26,11 @@ class Public::CustomersController < ApplicationController
 
   def followers
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :first_name_kana, :last_name, :last_name_kana, :postcode, :address, :telephone_number, :email)
+  end
+
 end
