4818e5444dff14bfca8f577c2384f24f9ec420ab
Masuda-Jion <masuda0311jion@gmail.com>
2023-04-15 01:39:56 +0000

    [Start] カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,53 @@
 class Public::CartItemsController < ApplicationController
+
+  before_action :authenticate_customer!
+
   def index
+    @cart_items = current_customer.cart_items
   end
+
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+
+    if current_customer.cart_items.find_by(menu_id: params[:cart_item][:menu_id]).present?
+      cart_item = current_customer.cart_items.find_by(menu_id: params[:cart_item][:menu_id])
+      cart_item.count += params[:cart_item][:count].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    # もしカート内に「同じ」商品がない場合は通常の保存処理
+    elsif @cart_item.save
+      redirect_to cart_items_path
+    # 保存できなかった場合
+    else
+      render 'index'
+    end
+
+  end
+
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to request.referer
+  end
+
+  def destroy_all
+    cart_items = CartItem.all
+    CartItem.destroy_all
+    redirect_to cart_items_path
+  end
+
+
+
+  private
+
+  def cart_item_params
+      params.require(:cart_item).permit(:menu_id, :price, :count)
+  end
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,12 @@
 class CartItem < ApplicationRecord
+  
   #アソシエーション
   belongs_to :customer
   belongs_to :menu
+
+  # 小計計算
+  def subtotal_price
+    menu.tax_in_price * (self.count || 0)
+  end
+  
 end
