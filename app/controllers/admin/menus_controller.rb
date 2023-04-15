bb5065657ee323b835374c576d43cd5acc028a73
Masuda-Jion <masuda0311jion@gmail.com>
2023-04-15 01:06:30 +0000

    [Add] 商品登録(管理者)

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -1,13 +1,51 @@
 class Admin::MenusController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def new
+    @menu = Menu.new
   end
 
   def index
+    @menus = Menu.all.page(params[:page]).per(5)
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+
+    if @menu.save
+     redirect_to admin_menu_path(@menu)
+     flash[:notice] = "新規登録確認しました。"
+    else
+      render:new
+    end
+
   end
 
   def show
+    @menu = Menu.find(params[:id])
   end
 
   def edit
+    @menu = Menu.find(params[:id])
   end
+
+  def update
+    @menu = Menu.find(params[:id])
+
+    if @menu.update(menu_params)
+     redirect_to admin_menu_path(@menu.id)
+     flash[:notice] = "変更を保存しました。"
+    else
+      render:edit
+    end
+
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:genre_id, :name, :info,:non_tax_price, :is_sale, :image)
+  end
+
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,35 @@
 class Menu < ApplicationRecord
-  #アソシエーション
+  
+  # アソシエーション
   has_many :cart_items , dependent: :destroy
   has_many :order_details
   has_many :reviews , dependent: :destroy
   belongs_to :genre
+  
+  # バリデーション
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :non_tax_price, presence: true
+  validates :genre_id, presence: true
+  validates :is_sale, inclusion: {in: [true, false]}
+  
+  # 商品の画像
+  has_one_attached :image
+  
+  def get_image(width, height)
+    
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+    end
+    
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  # 税込価格
+  def tax_in_price
+     (self.non_tax_price * 1.1).round
+  end
+  
 end
