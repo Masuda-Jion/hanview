1265915ec2a268cf24fdcb5c51a4ff86aa76dc99
Masuda-Jion <masuda0311jion@gmail.com>
2023-04-15 15:01:33 +0000

    [Add] 会員情報編集(管理者側)

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,37 @@
 class Admin::CustomersController < ApplicationController
+  
+  before_action :authenticate_admin!
+  
   def index
+    @customers = Customer.all.page(params[:page]).per(5)
   end
 
   def show
+    @customers = Customer.all
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+  
+    if @customer.update(customer_params)
+      flash[:notice] = "変更を保存しました"
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
+  
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
+     :postcode, :address, :telephone_number, :email, :is_delete)
+  end
+
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,10 @@
 class Admin::HomesController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def top
+    @orders = Order.all.page(params[:page]).per(5)
+    @order_details = OrderDetail.all
   end
+
 end
