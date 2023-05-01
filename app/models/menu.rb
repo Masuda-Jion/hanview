77bad6b56b082c46606da0786a9bddbe418c6fb6
Masuda-Jion <masuda0311jion@gmail.com>
2023-05-01 16:45:26 +0000

    [Add] 画像サイズ

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -41,7 +41,7 @@ class Menu < ApplicationRecord
       image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
     end
 
-    image.variant(resize_to_limit: [width, height]).processed
+    image.variant( resize: "140" )
   end
 
   # 税込価格
